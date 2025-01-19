32fd04962474ebef500f416d60fa9d0b65f678b0
Darshan.V <darshanv487@gmail.com>
2025-01-19 19:58:30 +0530

    added the protected routes using clerk useAuth hook and have the link in the home to dashboard

--- chatgpt-clone/src/layouts/dashboardLayout/DashboardLayout.jsx
+++ chatgpt-clone/src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,7 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./dashboardLayout.css";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
 
 const DashboardLayout = () => {
+  const { userID, isLoaded } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoaded && !userID) {
+      navigate("/sign-in");
+    }
+  }, [isLoaded, userID, navigate]);
+
+  if (!isLoaded) {
+    return "Loading....";
+  }
   return (
     <div className="dashboardLayout">
       <div className="menu">MENU</div>
--- chatgpt-clone/src/routes/signinpage/SignInPage.jsx
+++ chatgpt-clone/src/routes/signinpage/SignInPage.jsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react";
 
 const SignInPage = () => {
   return (
-    <div>
+    <div className="signInPage">
       <SignIn path="/sign-in" />
     </div>
   );
