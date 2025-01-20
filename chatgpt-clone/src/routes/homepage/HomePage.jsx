92f3d7734e6a8326563f23e9cf3e027d24be2f06
Darshan.V <darshanv487@gmail.com>
2025-01-20 10:45:20 +0530

    chatgpt-clone/src/routes/homepage/HomePage.jsx: created the home page component

--- chatgpt-clone/src/routes/homepage/HomePage.jsx
+++ chatgpt-clone/src/routes/homepage/HomePage.jsx
@@ -1,10 +1,27 @@
-import { Link } from "react-router-dom";
 import "./HomePage.css";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div className="homePage">
-      <Link to="/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt=""  className="orbital"/>
+      <div className="left">
+        <h1>LAMA AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+          laborum libero amet provident quos nam tempore possimus.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+        </div>
+      </div>
     </div>
   );
 };
