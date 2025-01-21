4f0ea568ca923732465eef18ab105b25daa1bbfc
Darshan.V <darshanv487@gmail.com>
2025-01-21 12:56:27 +0530

    chatgpt-clone/src/components/newPrompt/NewPrompt.jsx: added the html and css for new prompt component and removed the smooth scroll effect from the chat page and implemented in the newprompt component

--- chatgpt-clone/src/components/newPrompt/NewPrompt.jsx
+++ chatgpt-clone/src/components/newPrompt/NewPrompt.jsx
@@ -1,9 +1,30 @@
-import './newPrompt.css'
+import { useEffect, useRef } from "react";
+import "./newPrompt.css";
 
 const NewPrompt = () => {
-  return (
-    <div className='newPrompt'>NewPrompt</div>
-  )
-}
+  const endRef = useRef(null);
 
-export default NewPrompt
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+  return (
+    <>
+      TEST
+      <div className="endChat" ref={endRef}></div>
+      <div className="newPrompt">
+        <form className="newForm">
+          <label htmlFor="file">
+            <img src="/attachment.png" alt="" />
+          </label>
+          <input id="file" type="file" multiple={false} hidden />
+          <input type="text" placeholder="Ask me anything..." />
+          <button>
+            <img src="/arrow.png" alt="" />
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default NewPrompt;
--- chatgpt-clone/src/routes/chatpage/ChatPage.jsx
+++ chatgpt-clone/src/routes/chatpage/ChatPage.jsx
@@ -3,11 +3,6 @@ import "./ChatPage.css";
 import NewPrompt from "../../components/newPrompt/NewPrompt";
 
 const ChatPage = () => {
-  const endRef = useRef(null);
-
-  useEffect(() => {
-    endRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, []);
   return (
     <div className="chatPage">
       <div className="wrapper">
@@ -50,7 +45,6 @@ const ChatPage = () => {
           <div className="message">Test message from ai</div>
           <div className="message user">Test message from user</div>
           <NewPrompt />
-          <div ref={endRef} />
         </div>
       </div>
     </div>
