8757d2a7d6f146329e26fbba2c9aafb37419e21e
Darshan.V <darshanv487@gmail.com>
2025-01-21 14:09:31 +0530

    installed express and ran the server on the port 3000 and tested a server whether it is working or not

--- chatgpt-clone/backend/index.js
+++ chatgpt-clone/backend/index.js
@@ -1,1 +1,12 @@
-console.log("test2");
+import express from "express";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get("/api/upload", (req, res) => {
+  res.send("It works!!");
+});
+
+app.listen(port, () => {
+  console.log("Server is listening on 3000");
+});
