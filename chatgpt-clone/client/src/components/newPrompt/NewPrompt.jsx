587b8d02dfce20db6de2480abf0c6e8dd463c53e
Darshan.V <darshanv487@gmail.com>
2025-01-21 15:52:37 +0530

    made the image render in the chat when we choose the file and sent it

--- chatgpt-clone/client/src/components/newPrompt/NewPrompt.jsx
+++ chatgpt-clone/client/src/components/newPrompt/NewPrompt.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./newPrompt.css";
 import Upload from "../upload/Upload";
+import { IKImage } from "imagekitio-react";
 
 const NewPrompt = () => {
   const endRef = useRef(null);
@@ -8,13 +9,27 @@ const NewPrompt = () => {
   useEffect(() => {
     endRef.current?.scrollIntoView({ behavior: "smooth" });
   }, []);
+
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+  });
   return (
     <>
-      TEST
+      {img.isLoading && <div className="">Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={[{ width: 380 }]}
+        />
+      )}
       <div className="endChat" ref={endRef}></div>
       <div className="newPrompt">
         <form className="newForm">
-          <Upload />
+          <Upload setImg={setImg} />
           <input id="file" type="file" multiple={false} hidden />
           <input type="text" placeholder="Ask me anything..." />
           <button>
