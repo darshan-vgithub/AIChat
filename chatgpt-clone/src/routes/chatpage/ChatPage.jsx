a5dc37f3458c7a323bedd906272410eb29e193d6
Darshan.V <darshanv487@gmail.com>
2025-01-21 12:16:27 +0530

    chatgpt-clone/src/routes/chatpage/ChatPage.jsx: added the html and css for chatpage and made it presentable

--- chatgpt-clone/src/routes/chatpage/ChatPage.jsx
+++ chatgpt-clone/src/routes/chatpage/ChatPage.jsx
@@ -2,8 +2,48 @@ import "./ChatPage.css";
 
 const ChatPage = () => {
   return (
-    <div>
-      <h1>Chat Page</h1>
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from ai</div>
+          <div className="message user">
+            Test message from user Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Culpa odit rem ipsa ipsum corporis illum, expedita
+            harum vitae adipisci sed omnis id voluptatum amet consequuntur
+            sapiente doloremque corrupti iure modi.
+          </div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+        </div>
+      </div>
     </div>
   );
 };
