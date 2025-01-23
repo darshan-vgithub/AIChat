da9f22709cded97647729b5af7789a62848ad01c
Darshan.V <darshanv487@gmail.com>
2025-01-23 15:59:29 +0530

    now connected express to mongodb using mongoose and used mongodb shell instead of atlas

--- chatgpt-clone/backend/index.js
+++ chatgpt-clone/backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import ImageKit from "imagekit";
+import mongoose from "mongoose";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -8,7 +9,10 @@ const app = express();
 app.use(
   cors({
     origin: (origin, callback) => {
-      const allowedOrigins = ["http://localhost:5173", "http://localhost:5173/"];
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "http://localhost:5173/",
+      ];
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
@@ -19,6 +23,14 @@ app.use(
   })
 );
 
+const Connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
   publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
@@ -31,5 +43,6 @@ app.get("/api/upload", (req, res) => {
 });
 
 app.listen(port, () => {
+  Connect()
   console.log(`Server is listening on ${port}`);
 });
